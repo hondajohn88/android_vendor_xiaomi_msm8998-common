5873ecfb9f2c2d1113f31c98fb94c9c9c299ee5d
hondajohn88 <hondajohn88@gmail.com>
2018-01-05 15:03:07 -0800

    Add libqminvapi to packages.

--- Android.mk
+++ Android.mk
@@ -237,4 +237,16 @@ LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libqminvapi
+LOCAL_MODULE_OWNER := xiaomi
+LOCAL_SRC_FILES_32 := proprietary/lib/libqminvapi.so
+LOCAL_SRC_FILES_64 := proprietary/lib64/libqminvapi.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
 endif
